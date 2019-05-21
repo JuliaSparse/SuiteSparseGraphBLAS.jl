27a1e3c3cd98d813972d7c7aedf6b5bef50ebdf4
Abhinav Mehndiratta <abhinavmehndiratta@gmail.com>
2019-05-21 21:44:59 +0530

    remove conversion to Cint

--- src/Context_Methods.jl
+++ src/Context_Methods.jl
@@ -1,7 +1,7 @@
 import Printf.@printf
 
 function GrB_init(mode::GrB_Mode)
-    return GrB_Info(ccall(dlsym(graphblas_lib, "GrB_init"), Cint, (Cint, ), Cint(mode)))
+    return GrB_Info(ccall(dlsym(graphblas_lib, "GrB_init"), Cint, (Cint, ), mode))
 end
 
 function GrB_wait()
