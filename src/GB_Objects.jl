b4621949c81db037124140ab84ade7ecc1e42c30
Abhinav Mehndiratta <abhinavmehndiratta@gmail.com>
2019-05-21 14:45:14 +0530

    create GrB_Type, GrB_UnaryOp and GrB_BinaryOp structures

---
+++ src/GB_Objects.jl
@@ -0,0 +1,20 @@
+import Base.show
+
+export GrB_Type, GrB_UnaryOp, GrB_BinaryOp
+
+mutable struct GrB_Type
+    p::Ptr{Cvoid}
+end
+Base.show(io::IO, ::GrB_Type) = print("GrB_Type")
+
+mutable struct GrB_UnaryOp
+    p::Ptr{Cvoid}
+end
+GrB_UnaryOp() = GrB_UnaryOp(Ptr{Cvoid}(0))
+Base.show(io::IO, ::GrB_UnaryOp) = print("GrB_UnaryOp")
+
+mutable struct GrB_BinaryOp
+    p::Ptr{Cvoid}
+end
+GrB_BinaryOp() = GrB_BinaryOp(Ptr{Cvoid}(0))
+Base.show(io::IO, ::GrB_BinaryOp) = print("GrB_BinaryOp")
