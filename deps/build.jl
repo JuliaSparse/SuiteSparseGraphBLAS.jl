7bf3ae87c1bf66d43ad04542021ce8d6bd9937fb
Abhinav Mehndiratta <abhinavmehndiratta@gmail.com>
2019-05-23 01:45:35 +0530

    fix indentation

--- deps/build.jl
+++ deps/build.jl
@@ -10,7 +10,7 @@ products = [
 bin_prefix = "https://raw.githubusercontent.com/abhinavmehndiratta/GB_bin/master"
 
 download_info = Dict(
-    Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc7)) => ("$bin_prefix/GraphBLAS.v2.3.2.x86_64-linux-gnu-gcc7.tar.gz", "177e043f840a30efd8e0e3614af41d6c99ab98da198bfda6f6b8c5239eb92785"),
+        Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc7)) => ("$bin_prefix/GraphBLAS.v2.3.2.x86_64-linux-gnu-gcc7.tar.gz", "177e043f840a30efd8e0e3614af41d6c99ab98da198bfda6f6b8c5239eb92785"),
 	Linux(:aarch64, libc=:glibc, compiler_abi=CompilerABI(:gcc7)) => ("$bin_prefix/GraphBLAS.v2.3.2.aarch64-linux-gnu-gcc7.tar.gz", "c4de51db6489a2d37f051402deb74a1c7dea7fe6e616a3de621d6f15a3890ae0"),
 	Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc8)) => ("$bin_prefix/GraphBLAS.v2.3.2.x86_64-linux-gnu-gcc8.tar.gz", "06c0bd7dd0868a9d34f1ef216a805ab4ef1e510b84965fc1e3f193ff105969b5"),
 	Linux(:aarch64, libc=:glibc, compiler_abi=CompilerABI(:gcc8)) => ("$bin_prefix/GraphBLAS.v2.3.2.aarch64-linux-gnu-gcc8.tar.gz", "13f5d707b547ee74173f324cafc27620c3fea377b5bf80e176c7869aa022ae3c"),
