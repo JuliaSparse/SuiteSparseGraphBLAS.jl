565495be149b9feaaa6cc8ff7f3319893072f97d
Abhinav Mehndiratta <abhinavmehndiratta@gmail.com>
2019-05-24 16:05:52 +0530

    add examples in documentation for basic matrix methods

--- src/Object_Methods/Matrix_Methods.jl
+++ src/Object_Methods/Matrix_Methods.jl
@@ -2,6 +2,20 @@
     GrB_Matrix_new(A, type, nrows, ncols)
 
 Create a new matrix with specified domain and dimensions.
+
+# Examples
+```jldoctest
+julia> using SuiteSparseGraphBLAS
+
+julia> GrB_init(GrB_NONBLOCKING)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> MAT = GrB_Matrix()
+GrB_Matrix
+
+julia> GrB_Matrix_new(MAT, GrB_INT8, 4, 4)
+GrB_SUCCESS::GrB_Info = 0
+```
 """
 function GrB_Matrix_new(A::GrB_Matrix, type::GrB_Type, nrows::T, ncols::T) where T <: GrB_Index
     A_ptr = pointer_from_objref(A)
@@ -20,6 +34,44 @@ end
     GrB_Matrix_build(C, I, J, X, nvals, dup)
 
 Store elements from tuples into a vector.
+
+# Examples
+```jldoctest
+julia> using SuiteSparseGraphBLAS
+
+julia> GrB_init(GrB_NONBLOCKING)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> MAT = GrB_Matrix()
+GrB_Matrix
+
+julia> GrB_Matrix_new(MAT, GrB_INT8, 4, 4)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> I = [1, 2, 2, 2, 3]; J = [1, 2, 1, 3, 3]; X = Int8[2, 3, 4, 5, 6]; n = 5;
+
+julia> GrB_Matrix_build(MAT, I, J, X, n, GrB_FIRST_INT8)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> @GxB_Matrix_fprint(MAT, GxB_COMPLETE)
+
+GraphBLAS matrix: MAT
+nrows: 4 ncols: 4 max # entries: 5
+format: standard CSR vlen: 4 nvec_nonempty: 3 nvec: 4 plen: 4 vdim: 4
+hyper_ratio 0.0625
+GraphBLAS type:  int8_t size: 1
+number of entries: 5
+row: 1 : 1 entries [0:0]
+    column 1: int8 2
+row: 2 : 3 entries [1:3]
+    column 1: int8 4
+    column 2: int8 3
+    column 3: int8 5
+row: 3 : 1 entries [4:4]
+    column 3: int8 6
+
+
+```
 """
 function GrB_Matrix_build(C::GrB_Matrix, I::Vector{U}, J::Vector{U}, X::Vector{T}, nvals::U, dup::GrB_BinaryOp) where{U <: GrB_Index, T <: valid_types}
     I_ptr = pointer(I)
@@ -41,6 +93,23 @@ end
 
 Return the number of rows in a matrix if successful.
 Else return value of type GrB Info.
+
+# Examples
+```jldoctest
+julia> using SuiteSparseGraphBLAS
+
+julia> GrB_init(GrB_NONBLOCKING)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> MAT = GrB_Matrix()
+GrB_Matrix
+
+julia> GrB_Matrix_new(MAT, GrB_INT8, 4, 4)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> GrB_Matrix_nrows(MAT)
+4
+```
 """
 function GrB_Matrix_nrows(A::GrB_Matrix)
     nrows = Ref(UInt64(0))
@@ -61,6 +130,23 @@ end
 
 Return the number of columns in a matrix if successful.
 Else return value of type GrB Info.
+
+# Examples
+```jldoctest
+julia> using SuiteSparseGraphBLAS
+
+julia> GrB_init(GrB_NONBLOCKING)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> MAT = GrB_Matrix()
+GrB_Matrix
+
+julia> GrB_Matrix_new(MAT, GrB_INT8, 4, 4)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> GrB_Matrix_ncols(MAT)
+4
+```
 """
 function GrB_Matrix_ncols(A::GrB_Matrix)
     ncols = Ref(UInt64(0))
@@ -81,6 +167,28 @@ end
 
 Return the number of stored elements in a matrix if successful.
 Else return value of type GrB Info.
+
+# Examples
+```jldoctest
+julia> using SuiteSparseGraphBLAS
+
+julia> GrB_init(GrB_NONBLOCKING)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> MAT = GrB_Matrix()
+GrB_Matrix
+
+julia> GrB_Matrix_new(MAT, GrB_INT8, 4, 4)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> I = [1, 2, 2, 2, 3]; J = [1, 2, 1, 3, 3]; X = Int8[2, 3, 4, 5, 6]; n = 5;
+
+julia> GrB_Matrix_build(MAT, I, J, X, n, GrB_FIRST_INT8)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> GrB_Matrix_nvals(MAT)
+5
+```
 """
 function GrB_Matrix_nvals(A::GrB_Matrix)
     nvals = Ref(UInt64(0))
@@ -100,6 +208,49 @@ end
     GrB_Matrix_dup(C, A)
 
 Create a new matrix with the same domain, dimensions, and contents as another matrix.
+
+# Examples
+```jldoctest
+julia> using SuiteSparseGraphBLAS
+
+julia> GrB_init(GrB_NONBLOCKING)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> MAT = GrB_Matrix()
+GrB_Matrix
+
+julia> GrB_Matrix_new(MAT, GrB_INT8, 4, 4)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> I = [1, 2, 2, 2, 3]; J = [1, 2, 1, 3, 3]; X = Int8[2, 3, 4, 5, 6]; n = 5;
+
+julia> GrB_Matrix_build(MAT, I, J, X, n, GrB_FIRST_INT8)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> B = GrB_Matrix()
+GrB_Matrix
+
+julia> GrB_Matrix_dup(B, MAT)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> @GxB_Matrix_fprint(B, GxB_SHORT)
+
+GraphBLAS matrix: B
+nrows: 4 ncols: 4 max # entries: 5
+format: standard CSR vlen: 4 nvec_nonempty: 3 nvec: 4 plen: 4 vdim: 4
+hyper_ratio 0.0625
+GraphBLAS type:  int8_t size: 1
+number of entries: 5
+row: 1 : 1 entries [0:0]
+    column 1: int8 2
+row: 2 : 3 entries [1:3]
+    column 1: int8 4
+    column 2: int8 3
+    column 3: int8 5
+row: 3 : 1 entries [4:4]
+    column 3: int8 6
+
+```
 """
 function GrB_Matrix_dup(C::GrB_Matrix, A::GrB_Matrix)
     C_ptr = pointer_from_objref(C)
@@ -118,6 +269,34 @@ end
     GrB_Matrix_clear(A)
 
 Remove all elements from a matrix.
+
+# Examples
+```jldoctest
+julia> using SuiteSparseGraphBLAS
+
+julia> GrB_init(GrB_NONBLOCKING)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> MAT = GrB_Matrix()
+GrB_Matrix
+
+julia> GrB_Matrix_new(MAT, GrB_INT8, 4, 4)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> I = [1, 2, 2, 2, 3]; J = [1, 2, 1, 3, 3]; X = Int8[2, 3, 4, 5, 6]; n = 5;
+
+julia> GrB_Matrix_build(MAT, I, J, X, n, GrB_FIRST_INT8)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> GrB_Matrix_nvals(MAT)
+5
+
+julia> GrB_Matrix_clear(MAT)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> GrB_Matrix_nvals(MAT)
+0
+```
 """
 function GrB_Matrix_clear(A::GrB_Matrix)
     return GrB_Info(
@@ -134,6 +313,35 @@ end
     GrB_Matrix_setElement(C, X, I, J)
 
 Set one element of a matrix to a given value, C[I][J] = X.
+
+# Examples
+```jldoctest
+julia> using SuiteSparseGraphBLAS
+
+julia> GrB_init(GrB_NONBLOCKING)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> MAT = GrB_Matrix()
+GrB_Matrix
+
+julia> GrB_Matrix_new(MAT, GrB_INT8, 4, 4)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> I = [1, 2, 2, 2, 3]; J = [1, 2, 1, 3, 3]; X = Int8[2, 3, 4, 5, 6]; n = 5;
+
+
+julia> GrB_Matrix_build(MAT, I, J, X, n, GrB_FIRST_INT8)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> GrB_Matrix_extractElement(MAT, 1, 1)
+2
+
+julia> GrB_Matrix_setElement(MAT, Int8(7), 1, 1)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> GrB_Matrix_extractElement(MAT, 1, 1)
+7
+```
 """
 function GrB_Matrix_setElement(C::GrB_Matrix, X::T, I::U, J::U) where {U <: GrB_Index, T <: valid_int_types}
     fn_name = "GrB_Matrix_setElement_" * get_suffix(T)
@@ -176,6 +384,28 @@ end
 
 Return element of a vector at a given index (A[row_index][col_index]) if successful.
 Else return value of type GrB Info.
+
+# Examples
+```jldoctest
+julia> using SuiteSparseGraphBLAS
+
+julia> GrB_init(GrB_NONBLOCKING)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> MAT = GrB_Matrix()
+GrB_Matrix
+
+julia> GrB_Matrix_new(MAT, GrB_INT8, 4, 4)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> I = [1, 2, 2, 2, 3]; J = [1, 2, 1, 3, 3]; X = Int8[2, 3, 4, 5, 6]; n = 5;
+
+julia> GrB_Matrix_build(MAT, I, J, X, n, GrB_FIRST_INT8)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> GrB_Matrix_extractElement(MAT, 1, 1)
+2
+```
 """
 function GrB_Matrix_extractElement(A::GrB_Matrix, row_index::U, col_index::U) where U <: GrB_Index
     res, A_type = GxB_Matrix_type(A)
@@ -200,6 +430,28 @@ end
     GrB_Matrix_extractTuples(A)
 
 Return tuples stored in a matrix.
+
+# Examples
+```jldoctest
+julia> using SuiteSparseGraphBLAS
+
+julia> GrB_init(GrB_NONBLOCKING)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> MAT = GrB_Matrix()
+GrB_Matrix
+
+julia> GrB_Matrix_new(MAT, GrB_INT8, 4, 4)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> I = [1, 2, 2, 2, 3]; J = [1, 2, 1, 3, 3]; X = Int8[2, 3, 4, 5, 6]; n = 5;
+
+julia> GrB_Matrix_build(MAT, I, J, X, n, GrB_FIRST_INT8)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> GrB_Matrix_extractTuples(MAT)
+([1, 2, 2, 2, 3], [1, 1, 2, 3, 3], Int8[2, 4, 3, 5, 6])
+```
 """
 function GrB_Matrix_extractTuples(A::GrB_Matrix)
     res, A_type = GxB_Matrix_type(A)
