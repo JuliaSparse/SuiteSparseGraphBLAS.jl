7c9a426d78f4296bf90acd0ea688f0fe014ba20f
Abhinav Mehndiratta <abhinavmehndiratta@gmail.com>
2019-05-28 19:45:32 +0530

    update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -10,7 +10,7 @@ bin_prefix = "https://raw.githubusercontent.com/abhinavmehndiratta/GB_bin/master
 
 download_info = Dict(
     Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc7)) => ("$bin_prefix/GraphBLAS.v2.2.3.x86_64-linux-gnu-gcc7.tar.gz", "16ff3800e51623f2379f0eebf999964c18e4c38efa5430008a51d4ddbc527cd2"),
-	Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc8)) => ("$bin_prefix/GraphBLAS.v2.2.3.x86_64-linux-gnu-gcc8.tar.gz", "1379bc3198d145a56945f276ddf71320336f1af9df46292e2704f0e538f8938e"),
+    Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc8)) => ("$bin_prefix/GraphBLAS.v2.2.3.x86_64-linux-gnu-gcc8.tar.gz", "1379bc3198d145a56945f276ddf71320336f1af9df46292e2704f0e538f8938e"),
 )
 
 if any(!satisfied(p; verbose=verbose) for p in products)
