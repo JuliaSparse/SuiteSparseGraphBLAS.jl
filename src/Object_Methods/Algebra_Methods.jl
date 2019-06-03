57daf4cac165beff77f4e344320714b214c8f42c
Abhinav Mehndiratta <abhinavmehndiratta@gmail.com>
2019-06-03 18:00:36 +0530

    add documentation for unary operator

--- src/Object_Methods/Algebra_Methods.jl
+++ src/Object_Methods/Algebra_Methods.jl
@@ -73,9 +73,61 @@ function GrB_BinaryOp_new(
 end
 
 """
-    GrB_UnaryOp_new(op, fn, ztype, xtype, ytype)
+    GrB_UnaryOp_new(op, fn, ztype, xtype)
 
 Initialize a new GraphBLAS unary operator with a specified user-defined function and its types.
+
+# Examples
+```jldoctest
+julia> using SuiteSparseGraphBLAS
+
+julia> GrB_init(GrB_NONBLOCKING)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> u = GrB_Vector{Int64}()
+GrB_Vector{Int64}
+
+julia> GrB_Vector_new(u, GrB_INT64, 3)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> I = [0, 2]; X = [10, 20]; n = 2;
+
+julia> GrB_Vector_build(u, I, X, n, GrB_FIRST_INT64)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> w = GrB_Vector{Int64}()
+GrB_Vector{Int64}
+
+julia> GrB_Vector_new(w, GrB_INT64, 3)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> function NEG(a)
+           return -a
+       end
+NEG (generic function with 1 method)
+
+julia> negative = GrB_UnaryOp()
+GrB_UnaryOp
+
+julia> GrB_UnaryOp_new(negative, NEG, GrB_INT64, GrB_INT64)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> GrB_apply(w, GrB_NULL, GrB_NULL, negative, u, GrB_NULL)
+GrB_SUCCESS::GrB_Info = 0
+
+julia> @GxB_fprint(w, GxB_COMPLETE)
+
+GraphBLAS vector: w 
+nrows: 3 ncols: 1 max # entries: 2
+format: standard CSC vlen: 3 nvec_nonempty: 1 nvec: 1 plen: 1 vdim: 1
+hyper_ratio 0.0625
+GraphBLAS type:  int64_t size: 8
+number of entries: 2 
+column: 0 : 2 entries [0:1]
+    row 0: int64 -10
+    row 2: int64 -20
+
+```
 """
 function GrB_UnaryOp_new(
     op::GrB_UnaryOp,
