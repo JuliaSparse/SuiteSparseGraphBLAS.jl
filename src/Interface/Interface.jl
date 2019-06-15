565247a9c6dff41f4643ba95b86daf7e963f1dba
Abhinav Mehndiratta <abhinavmehndiratta@gmail.com>
2019-06-15 13:39:43 +0530

    add vector interface

--- src/Interface/Interface.jl
+++ src/Interface/Interface.jl
@@ -6,9 +6,10 @@ import Base:
     getindex, setindex!, empty!, copy, size
 
 import SuiteSparseGraphBLAS:
-    GrB_Info, GrB_Index, GrB_Matrix, valid_types, get_GrB_Type, default_dup
+    GrB_Info, GrB_Index, GrB_Matrix, GrB_Vector, valid_types, get_GrB_Type, default_dup
 
 include("./Object_Methods/Matrix_Methods.jl")
+include("./Object_Methods/Vector_Methods.jl")
 export findnz, nnz
 
 end # end of module
