2003de838415f3de160e421d997756e95dd43dbb
Abhinav Mehndiratta <abhinavmehndiratta@gmail.com>
2019-06-15 23:41:53 +0530

    add interface for descriptor methods

--- src/Interface/Interface.jl
+++ src/Interface/Interface.jl
@@ -6,10 +6,12 @@ import Base:
     getindex, setindex!, empty!, copy, size
 
 import SuiteSparseGraphBLAS:
-    GrB_Info, GrB_Index, GrB_Matrix, GrB_Vector, valid_types, get_GrB_Type, default_dup
+    GrB_Info, GrB_Index, GrB_Matrix, GrB_Vector, GrB_Descriptor, GrB_Desc_Field, GrB_Desc_Value,
+    valid_types, get_GrB_Type, default_dup
 
 include("./Object_Methods/Matrix_Methods.jl")
 include("./Object_Methods/Vector_Methods.jl")
+include("./Object_Methods/Descriptor_Methods.jl")
 export findnz, nnz
 
 end # end of module
