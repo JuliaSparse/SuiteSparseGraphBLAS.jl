c9e855032ee13c063f9c4da87fcac635b26dc93a
Abhinav Mehndiratta <abhinavmehndiratta@gmail.com>
2019-06-17 13:00:12 +0530

    fix documentation

--- src/Context_Methods.jl
+++ src/Context_Methods.jl
@@ -3,8 +3,8 @@ import Printf.@printf
 """
     GrB_init(mode)
 
-GrB_init must called before any other GraphBLAS operation.
-GrB_init defines the mode that GraphBLAS will use: blocking or non-blocking.
+`GrB_init` must called before any other GraphBLAS operation.
+`GrB_init` defines the mode that GraphBLAS will use: blocking or non-blocking.
 With blocking mode, all operations finish before returning to the user application.
 With non-blocking mode, operations can be left pending, and are computed only when needed.
 """
@@ -15,8 +15,8 @@ end
 """
     GrB_wait()
 
-GrB_wait forces all pending operations to complete.
-Blocking mode is as if GrB_wait is called whenever a GraphBLAS method or operation returns to the user.
+`GrB_wait` forces all pending operations to complete.
+Blocking mode is as if `GrB_wait` is called whenever a GraphBLAS method or operation returns to the user.
 """
 function GrB_wait()
     return GrB_Info(ccall(dlsym(graphblas_lib, "GrB_wait"), Cint, (), ))
@@ -25,8 +25,8 @@ end
 """
     GrB_finalize()
 
-GrB_finalize must be called as the last GraphBLAS operation.
-GrB_finalize does not call GrB_wait; any pending computations are abandoned.
+`GrB_finalize` must be called as the last GraphBLAS operation.
+`GrB_finalize` does not call `GrB_wait`; any pending computations are abandoned.
 """
 function GrB_finalize()
     return GrB_Info(ccall(dlsym(graphblas_lib, "GrB_finalize"), Cint, (), ))
@@ -35,8 +35,8 @@ end
 """
     GrB_error()
 
-Each GraphBLAS method and operation returns a GrB_Info error code.
-GrB_error returns additional information on the error.
+Each GraphBLAS method and operation returns a `GrB_Info` error code.
+`GrB_error` returns additional information on the error.
 
 # Examples
 ```jldoctest
