2fcf359282fc5f7b16b0905ed24ccfd29e27472d
Abhinav Mehndiratta <abhinavmehndiratta@gmail.com>
2019-06-21 17:17:25 +0530

    add vector and matrix methods tests (#5)

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,3 +2,16 @@ using SuiteSparseGraphBLAS
 using Test
 
 @test GrB_init(GrB_NONBLOCKING) == GrB_SUCCESS
+
+const testdir = dirname(@__FILE__)
+
+tests = [
+    "matrix_and_vector_methods",
+]
+
+@testset "SuiteSparseGraphBLAS" begin
+    for t in tests
+        tp = joinpath(testdir, "$(t).jl")
+        include(tp)
+    end
+end
