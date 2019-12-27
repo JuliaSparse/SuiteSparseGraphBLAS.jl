acf15bcff7eb9ef96522b5ad963d3069e106dcbb
Abhinav Mehndiratta <abhinavmehndiratta@gmail.com>
2019-12-27 12:02:27 +0530

    fix typo

--- src/Object_Methods/Matrix_Methods.jl
+++ src/Object_Methods/Matrix_Methods.jl
@@ -193,7 +193,7 @@ GrB_Matrix{Int8}
 julia> GrB_Matrix_new(MAT, GrB_INT8, 4, 4)
 GrB_SUCCESS::GrB_Info = 0
 
-julia> I = [ZeroBasedIndex1, 2, 2, 2, 3]; J = ZeroBasedIndex[1, 2, 1, 3, 3]; X = Int8[2, 3, 4, 5, 6]; n = 5;
+julia> I = ZeroBasedIndex[1, 2, 2, 2, 3]; J = ZeroBasedIndex[1, 2, 1, 3, 3]; X = Int8[2, 3, 4, 5, 6]; n = 5;
 
 julia> GrB_Matrix_build(MAT, I, J, X, n, GrB_FIRST_INT8)
 GrB_SUCCESS::GrB_Info = 0
