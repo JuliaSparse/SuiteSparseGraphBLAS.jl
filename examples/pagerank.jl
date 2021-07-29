0e982e756579d832ef07e87dc8bf3656532eafa9
Wimmerer <wrkimmerer@outlook.com>
2021-07-29 08:33:07 -0400

    move TC out to it's own file

--- examples/pagerank.jl
+++ examples/pagerank.jl
@@ -1,6 +1,4 @@
 using SuiteSparseGraphBLAS
-using MatrixMarket
-using SparseArrays
 function pagerank(
     A,
     d = reduce(Monoids.PLUS_MONOID, A; dims=2),
@@ -29,19 +27,4 @@ function pagerank(
         end
     end
     return r, i
-end
-
-
-function triangle_centrality1(A)
-    T = mul(A, A', Semirings.PLUS_TIMES[Float64], mask=A)
-    y = reduce(Monoids.PLUS_MONOID[Float64], T, dims=2)
-    k = reduce(Monoids.PLUS_MONOID[Float64], y)
-    return (3 * mul(A, y) - 2 * mul(T, y) .+ y) ./ k
-end
-
-function triangle_centrality2(A)
-    T = mul(A, A', Semirings.PLUS_PAIR, mask=A, desc=Descriptors.S)
-    y = reduce(Monoids.PLUS_MONOID[Float64], T, dims=2)
-    k = reduce(Monoids.PLUS_MONOID[Float64], y)
-    return map(BinaryOps.TIMES, eadd(eadd(3.0 * mul(A, y), 2.0 * mul(T, y, Semirings.PLUS_SECOND), BinaryOps.MINUS), y), 1/k)
 end
