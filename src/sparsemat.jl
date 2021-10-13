dc2d4c0bc3009729e18d183bcfc8f352be825388
Will Kimmerer <wrkimmerer@outlook.com>
2021-10-13 12:28:19 -0400

    broadcasting/mapping

--- src/sparsemat.jl
+++ src/sparsemat.jl
@@ -1,6 +1,11 @@
 module SparseArrayCompat
 import SparseArrays
+using ..SuiteSparseGraphBLAS
 using ..SuiteSparseGraphBLAS: GBMatrix
+using LinearAlgebra
+using Base.Broadcast
+# Basic struct, Base, and SparseArrays definitions:
+###################################################
 """
 SparseMatrixCSC compatible wrapper over GBMatrix.
 """
@@ -33,8 +38,9 @@ SparseMatrixGB(
     m = maximum(I), n = maximum(J)
 ) where {T} = 
     SparseMatrixGB(GBMatrix(I, J, x; nrows=m, ncols=n), fill)
-SparseMatrixGB(A::GBMatrix{T}) where {T} = SparseMatrixGB{T}(A, zero(T))
-
+SparseMatrixGB(A::GBMatrix{T}, fill=zero(T)) where {T} = SparseMatrixGB{T}(A, fill)
+SparseMatrixGB(A::SparseArrays.SparseMatrixCSC{T}, fill=zero(T)) where{T} =
+    SparseMatrixGB{T}(GBMatrix(A), fill)
 Base.copy(A::SparseMatrixGB{Tv}) where {Tv} = SparseMatrixGB(copy(A.gbmat), copy(A.fillvalue))
 Base.size(A::SparseMatrixGB) = size(A.gbmat)
 SparseArrays.nnz(A::SparseMatrixGB) = nnz(A.gbmat)
@@ -55,6 +61,107 @@ SparseArrays.findnz(A::SparseMatrixGB) = SparseArrays.findnz(A.gbmat)
 SparseArrays.nonzeros(A::SparseMatrixGB) = SparseArrays.nonzeros(A.gbmat)
 SparseArrays.nonzeroinds(A::SparseMatrixGB) = SparseArrays.nonzeroinds(A.gbmat)
 
-Base.show(io::IO, ::MIME"text/plain", A::SparseMatrixGB) = show(io, MIME"text/plain"(), A.gbmat)
+function Base.show(io::IO, ::MIME"text/plain", A::SparseMatrixGB) 
+    SuiteSparseGraphBLAS.gxbprint(io, A.gbmat, "fill value: $(A.fillvalue)")
+end
+
+# Math
+######
+Base.:+(A::SparseMatrixGB, B::SparseMatrixGB) = SparseMatrixGB(A.gbmat + B.gbmat, A.fillvalue + B.fillvalue)
+
+# This will, naturally, totally ignore fill values and use zero(T).
+# I have no idea what to put as the new fill value, so use the default
+Base.:*(A::SparseMatrixGB, B::SparseMatrixGB) = SparseMatrixGB(A.gbmat * B.gbmat)
+
+# Mapping
+function Base.map!(op, A::SparseMatrixGB) 
+    map!(op, A.gbmat)
+    A.fillvalue = op(A.fillvalue)
+end
+function Base.map!(op, C::SparseMatrixGB, A::SparseMatrixGB)
+    map!(op, C, A)
+    C.fillvalue = op(A.fillvalue)
+end
+
+function Base.map!(op, A::SparseMatrixGB, x)
+    map!(op, A.gbmat, x)
+    A.fillvalue = op(A.fillvalue, x)
+end
+function Base.map!(op, C::SparseMatrixGB, A::SparseMatrixGB, x)
+    map!(op, C, A, x)
+    C.fillvalue = op(A.fillvalue, x)
+end
+
+Base.map(op, A::SparseMatrixGB) = SparseMatrixGB(map(op, A.gbmat), op(A.fillvalue))
+Base.map(op, A::SparseMatrixGB, x) = SparseMatrixGB(map(op, A.gbmat, x), op(A.fillvalue, x))
+
+function SuiteSparseGraphBLAS.eadd!(C::SparseMatrixGB, A::SparseMatrixGB, B::SparseMatrixGB, op::Function) 
+    eadd!(C.gbmat, A.gbmat, B.gbmat)
+    C.fillvalue = op(A.fillvalue, B.fillvalue)
+end
+function SuiteSparseGraphBLAS.eadd(A::SparseMatrixGB, B::SparseMatrixGB, op::Function) 
+    return SparseMatrixGB(eadd(A.gbmat, B.gbmat, op), op(A.fillvalue, B.fillvalue))
+end
+
+function SuiteSparseGraphBLAS.emul!(C::SparseMatrixGB, A::SparseMatrixGB, B::SparseMatrixGB, op::Function) 
+    emul!(C.gbmat, A.gbmat, B.gbmat)
+    C.fillvalue = op(A.fillvalue, B.fillvalue)
+end
+function SuiteSparseGraphBLAS.emul(A::SparseMatrixGB, B::SparseMatrixGB, op::Function) 
+    return SparseMatrixGB(mul(A.gbmat, B.gbmat, op), op(A.fillvalue, B.fillvalue))
+end
+
+# Broadcasting
+# There's probably a better way to do this, but < 100 loc duplication is fine.
+# This should be kept in sync with the GBMatrix/GBVector broadcasting as much as possible.
+valunwrap(::Val{x}) where x = x
+#This is directly from the Broadcasting interface docs
+struct SparseMatGBStyle <: Broadcast.AbstractArrayStyle{2} end
+Base.BroadcastStyle(::Type{<:SparseMatrixGB}) = SparseMatGBStyle()
+Base.BroadcastStyle(::Type{<:Transpose{T, <:SparseMatrixGB} where T}) = SparseMatGBStyle()
+
+SparseMatGBStyle(::Val{0}) = SparseMatGBStyle()
+SparseMatGBStyle(::Val{1}) = SparseMatGBStyle()
+SparseMatGBStyle(::Val{2}) = SparseMatGBStyle()
+SparseMatGBStyle(::Val{N}) where N = Broadcast.DefaultArrayStyle{N}()
+
+function Base.similar(
+    bc::Broadcast.Broadcasted{SparseMatGBStyle},
+    ::Type{ElType}
+) where {ElType}
+    return SparseMatrixGB{ElType}(axes(bc))
+end
+
+@inline function Base.copy(bc::Broadcast.Broadcasted{SparseMatGBStyle})
+    f = bc.f
+    l = length(bc.args)
+    if l == 1
+        x = first(bc.args)
+        if x isa Broadcast.Broadcasted
+            x = copy(x)
+        end
+        return map(f, x)
+    else
+        left = first(bc.args)
+        right = last(bc.args)
+        if left isa Base.RefValue{typeof(^)}
+            f = ^
+            left = bc.args[2]
+            right = valunwrap(right[])
+        end
+        if left isa Broadcast.Broadcasted
+            left = copy(left)
+        end
+        if right isa Broadcast.Broadcasted
+            right = copy(right)
+        end
+        if left isa SparseMatrixGB && right isa SparseMatrixGB
+            add = SuiteSparseGraphBLAS.defaultadd(f)
+            return add(left, right, f)
+        else
+            return map(f, left, right)
+        end
+    end
+end
 
 end
