08e8445c88818a425da4382adff6ecdfdff190f8
Wimmerer <wrkimmerer@outlook.com>
2021-10-26 10:10:05 -0400

    add benchmark script

--- benchmarks/benchmarks.jl
+++ benchmarks/benchmarks.jl
@@ -1,35 +1,66 @@
 using Pkg
+Pkg.activate(".")
+Pkg.instantiate()
 using SuiteSparseMatrixCollection
 using MatrixMarket
 using SuiteSparseGraphBLAS
-SuiteSparseGraphBLAS.gbset(SuiteSparseGraphBLAS.FORMAT, SuiteSparseGraphBLAS.BYROW)
 using BenchmarkTools
 using DelimitedFiles
 using SparseArrays
 using LinearAlgebra
-Pkg.activate(".")
-Pkg.instantiate()
-ssmc = ssmc_db()
-matrices = filter(row ->  10000 <= row.nnz <= 15000, ssmc)
-paths = fetch_ssmc(matrices, format="MM")
-open("results.txt", "w") do io
+SuiteSparseGraphBLAS.gbset(SuiteSparseGraphBLAS.NTHREADS, 8)
+function benchmark(minsize, maxsize)
+    println("SuiteSparseGraphBLAS Threads: $(SuiteSparseGraphBLAS.gbget(SuiteSparseGraphBLAS.NTHREADS))")
+    ssmc = ssmc_db()
+    matrices = filter(row ->  (minsize <= row.nnz <= maxsize) && row.real==true, ssmc)
+    # THIS WILL DOWNLOAD THESE MATRICES. BE WARNED.
+    paths = fetch_ssmc(matrices, format="MM")
+    avgspeedupselfmultimes = 0
+    avgspeedupdensemattimes = 0
     for i ∈ 1:length(paths)
-        if !matrices[i, :real]
-            continue
-        end
-        GC.gc()
-        S = convert(SparseMatrixCSC{Float64}, MatrixMarket.mmread(joinpath(paths[i], "$(matrices[i,:name]).mtx")))
-        G = GBMatrix(S)
-        SuiteSparseGraphBLAS.gbset(G, SuiteSparseGraphBLAS.FORMAT, SuiteSparseGraphBLAS.BYROW)
-        diag(G)
-        m = rand(size(S, 2), 50)
-        m2 = GBMatrix(m)
-        selfmultimes1 = mean(@benchmark $S * ($S)').time
-        selfmultimes2 = mean(@benchmark $G * ($G)').time
+        name = matrices[i, :name]
+        println("$i/$(length(paths)) Matrix $name: ")
+        singlebench(joinpath(paths[i], "$name.mtx"))
+    end
+    x = avgspeedupselfmultimes
+    y = avgspeedupdensemattimes
+    println("Speedup of A * A': $(x / length(paths))")
+    println("Speedup of A * Full: $(y / length(paths))")
+end
 
-        densemattimes1 = mean(@benchmark $S * $m).time
-        densemattimes2 = mean(@benchmark $G * $m2).time
-        println(io, "$selfmultimes1\t$selfmultimes2\t$densemattimes1\t$densemattimes2")
-        println("Progress: $i")
+function singlebench(file)
+    GC.gc() #GC to be absolutely sure nothing is hanging around from last loop
+    S = convert(SparseMatrixCSC{Float64}, MatrixMarket.mmread(file))
+    G = GBMatrix(S)
+    # Set to row, this will likely be default in the future, and is the most performant.
+    SuiteSparseGraphBLAS.gbset(G, SuiteSparseGraphBLAS.FORMAT, SuiteSparseGraphBLAS.BYROW)
+    # Not sure if gbset is FORMAT is lazy, so to be sure.
+    diag(G)
+    # Fairly wide dense matrix for rhs.
+    m = rand(size(S, 2), 1000)
+    m2 = GBMatrix(m)
+    SuiteSparseGraphBLAS.gbset(SuiteSparseGraphBLAS.BURBLE, true)
+    println("--------------")
+    println("A * A':")
+    println("-------")
+    selfmultimes1 = @belapsed $S * ($S)' samples=1 evals=3
+    selfmultimes2 = @belapsed $G * ($G)' samples=1 evals=3
+    selfmultimesspeedup = selfmultimes1/selfmultimes2
+    println("\nSparseArrays=$selfmultimes1\t GraphBLAS=$selfmultimes2\t SA/GB=$selfmultimesspeedup\n")
+    println("A * Full:")
+    println("---------")
+    densemattimes1 = @belapsed $S * $m samples=1 evals=3
+    densemattimes2 = @belapsed $G * $m2 samples=1 evals=3
+    densemattimesspeedup = densemattimes1/densemattimes2
+    println("\nSparseArrays=$densemattimes1\t GraphBLAS=$densemattimes2\t SA/GB=$(densemattimes1/densemattimes2)\n")
+    SuiteSparseGraphBLAS.gbset(SuiteSparseGraphBLAS.BURBLE, false)
+end
+
+if length(ARGS) != 0
+    tryparse(Int64, ARGS[1])
+    if ARGS[1] === nothing #assume it's a path if not an integer
+        singlebench(ARGS[1])
+    else
+        (ARGS[1] isa Integer && ARGS[2] isa Integer && benchmark(ARGS[1], ARGS[2])) || benchmark(1000, 100000)
     end
 end
