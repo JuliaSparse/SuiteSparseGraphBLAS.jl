2792c18277135403c359ed49c141af59074ba845
Will Kimmerer <wrkimmerer@outlook.com>
2021-10-30 20:21:31 -0400

    Some updates to more recent API.

--- examples/Sandia.jl
+++ examples/Sandia.jl
@@ -12,13 +12,8 @@ using LinearAlgebra
 #A is the input matrix, Output: Triangle Count
 function sandia(A)
 
-    #extracting lower matrix from input matrix
     L = tril(A)
-
-
-    C = mul(L, L, Semirings.PLUS_TIMES, mask=L)
-    result = reduce(Monoids.PLUS_MONOID, C, dims=:)
-
-    return result
+    C = *(+, *; mask=L)(L, L)
+    return reduce(+, C, dims=:)
 
 end
