9de1baac0e9935a1d6ab140802661674d5118030
Will Kimmerer <wrkimmerer@outlook.com>
2022-02-24 03:42:37 -0500

    move FiniteDiff

--- src/chainrules/chainruleutils.jl
+++ src/chainrules/chainruleutils.jl
@@ -4,24 +4,5 @@ import ChainRulesCore: frule, rrule
 using ChainRulesCore
 const RealOrComplex = Union{Real, Complex}
 
-#Required for ChainRulesTestUtils
-function FiniteDifferences.to_vec(M::GBMatrix)
-    x, back = FiniteDifferences.to_vec(Matrix(M))
-    function backtomat(xvec)
-        M2 = GBMatrix(back(xvec))
-        return mask(M2, M; structural=true)
-    end
-    return x, backtomat
-end
-
-function FiniteDifferences.to_vec(v::GBVector)
-    x, back = FiniteDifferences.to_vec(Vector(v))
-    function backtovec(xvec)
-        v2 = GBVector(back(xvec))
-        return mask(v2, v; structural=true)
-    end
-    return x, backtovec
-end
-
 # LinearAlgebra.norm doesn't like the nothings.
 LinearAlgebra.norm(A::GBArray, p::Real=2) = norm(nonzeros(A), p)
--- test/chainrules/chainrulesutils.jl
+++ test/chainrules/chainrulesutils.jl
@@ -1,3 +1,22 @@
+#Required for ChainRulesTestUtils
+function FiniteDifferences.to_vec(M::GBMatrix)
+    x, back = FiniteDifferences.to_vec(Matrix(M))
+    function backtomat(xvec)
+        M2 = GBMatrix(back(xvec))
+        return mask(M2, M; structural=true)
+    end
+    return x, backtomat
+end
+
+function FiniteDifferences.to_vec(v::GBVector)
+    x, back = FiniteDifferences.to_vec(Vector(v))
+    function backtovec(xvec)
+        v2 = GBVector(back(xvec))
+        return mask(v2, v; structural=true)
+    end
+    return x, backtovec
+end
+
 function test_to_vec(x::T; check_inferred=true) where {T}
     check_inferred && @inferred FiniteDifferences.to_vec(x)
     x_vec, back = FiniteDifferences.to_vec(x)
