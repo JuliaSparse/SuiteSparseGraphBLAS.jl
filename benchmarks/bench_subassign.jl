e4f7f56d0d2cf6aa23af81a1067441abe98d401d
Will Kimmerer <wrkimmerer@outlook.com>
2022-04-15 04:19:01 -0400

    update bench script

--- benchmarks/bench_subassign.jl
+++ benchmarks/bench_subassign.jl
@@ -1,3 +1,4 @@
+println(stdout, "Opening Script")
 using Pkg
 Pkg.activate(".")
 Pkg.instantiate()
@@ -7,6 +8,7 @@ using SparseArrays
 using LinearAlgebra
 using StorageOrders
 using StatsBase
+println(stdout, "Loaded Packages")
 
 macro gbbench(ex)
     return quote
@@ -76,39 +78,44 @@ function runthreadedidx(C, A, I, J)
 end
 
 function singlebench(szC, szA)
+    gbresultsR = [0.0, 0.0, 0.0]
+    gbresultsC = [0.0, 0.0, 0.0]
+    SAresults = 0.0
     printstyled(stdout, "\nC($szC)[I, J] = A($szA))\n"; bold=true)
     println(stdout, "################################")
-    C = SuiteSparseGraphBLAS.gbrand(Float64, szC[1:2]..., szC[3])
-    A = SuiteSparseGraphBLAS.gbrand(Float64, szA[1:2]..., szA[3])
-    wait(C)
-    wait(A)
-    I = sample(1:size(C, 1), size(A, 1), replace = false)
-    J = sample(1:size(C, 2), size(A, 2), replace = false)
+    for i ∈ 1:10
+        C = SuiteSparseGraphBLAS.gbrand(Float64, szC[1:2]..., szC[3])
+        A = SuiteSparseGraphBLAS.gbrand(Float64, szA[1:2]..., szA[3])
+        wait(C)
+        wait(A)
+        I = sample(1:size(C, 1), size(A, 1), replace = false)
+        J = sample(1:size(C, 2), size(A, 2), replace = false)
 
-    flush(stdout)
-    gbset(A, :format, SuiteSparseGraphBLAS.BYROW)
-    gbset(C, :format, SuiteSparseGraphBLAS.BYROW)
-    SuiteSparseGraphBLAS.wait(A)
-    gbresultsR = runthreadedidx(C, A, I, J)
-    gbset(A, :format, SuiteSparseGraphBLAS.BYCOL)
-    gbset(C, :format, SuiteSparseGraphBLAS.BYCOL)
-    SuiteSparseGraphBLAS.wait(A)
-    gbresultsC = runthreadedidx(C, A, I, J)
-    A = SparseMatrixCSC(A) 
-    C = SparseMatrixCSC(C)
-    SAresults = idx(C, A, I, J)
+        flush(stdout)
+        gbset(A, :format, SuiteSparseGraphBLAS.BYROW)
+        gbset(C, :format, SuiteSparseGraphBLAS.BYROW)
+        SuiteSparseGraphBLAS.wait(A)
+        gbresultsR .+= runthreadedidx(C, A, I, J)
+        gbset(A, :format, SuiteSparseGraphBLAS.BYCOL)
+        gbset(C, :format, SuiteSparseGraphBLAS.BYCOL)
+        SuiteSparseGraphBLAS.wait(A)
+        gbresultsC .+= runthreadedidx(C, A, I, J)
+        A = SparseMatrixCSC(A) 
+        C = SparseMatrixCSC(C)
+        SAresults += idx(C, A, I, J)
+    end
     println(stdout, )
-    printstyled(stdout, "\nRESULTS, C[I, J] = A: \n"; bold=true, color=:green)
+    printstyled(stdout, "\nRESULTS, C($szC)[I, J] = A($szA): \n"; bold=true, color=:green)
     println(stdout, "################################")
-    println(stdout, "A by row (1, 2, 16 thread): $gbresultsR")
-    println(stdout, "A by col (1, 2, 16 thread): $gbresultsC")
-    println(stdout, "SparseArrays: $SAresults")
+    println(stdout, "A by row (1, 2, 16 thread): $(gbresultsR ./ 30)")
+    println(stdout, "A by col (1, 2, 16 thread): $(gbresultsC ./ 30)")
+    println(stdout, "SparseArrays: $(SAresults / 30)")
     flush(stdout)
     return nothing
 end
 
 singlebench((10_000, 10_000, 0.001), (2_000, 2_000, 0.1))
-singlebench((1_000_000, 1_000_000, 0.01), (5_000, 5_000, 0.005))
+#singlebench((1_000_000, 1_000_000, 0.01), (5_000, 5_000, 0.005))
 #singlebench((25_000_000, 25_000_000, 1e-7), (5_000, 5_000, 0.002))
 #singlebench((50_000_000, 50_000_000, 1e-7), (100_000, 100_000, 0.001))
 #
