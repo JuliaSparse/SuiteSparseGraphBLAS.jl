9aba30a09e54cc8ecec38f5baf2a9a755f7c4369
Wimmerer <kimmerer@mit.edu>
2022-04-18 08:02:57 -0400

    slightly rewrite iterator

--- src/iterator.jl
+++ src/iterator.jl
@@ -1,17 +1,19 @@
 struct GBIterator{O}
     p::Ref{LibGraphBLAS.GxB_Iterator}
+    A::AbstractGBArray
+    function GBIterator(A::AbstractGBArray)
+        #garbaage collection
+        i = Ref{LibGraphBLAS.GxB_Iterator}()
+        @wraperror LibGraphBLAS.GxB_Iterator_new(i)
+        p = finalizer(iter) do ref
+            @wraperror LibGraphBLAS.GxB_Iterator_free(ref)
+        end
+        return new{storageorder(A)}(p, A)
+    end
 end
 gbpointer(I::GBIterator) = I.p[]
 
-function GBIterator(bycol=true) # this should be removed eventually. We always want an attached one.
-    i = Ref{LibGraphBLAS.GxB_Iterator}()
-    @wraperror LibGraphBLAS.GxB_Iterator_new(i)
-    return GBIterator{}(finalizer(iter) do ref
-        @wraperror LibGraphBLAS.GxB_Iterator_free(ref)
-    end)
-end
-
-function attach(I::GBIterator, A::AbstractGBArray; desc = nothing, itercolumns)
+function _attach(I::GBIterator{O}, A::AbstractGBArray; desc = nothing) where {O}
     desc = _handledescriptor(desc)
     @wraperror LibGraphBLAS.GxB_Iterator_attach(gbpointer(I), gbpointer(A), desc)
 end
